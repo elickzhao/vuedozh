2b49e53221c67634be085d8ffdc8f9bd4d08f3a2
elick <xwiwi@foxmail.com>
2016-08-11 23:01:02 +0800

    首页国内bootcdn替换完毕

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -11,13 +11,13 @@
     <!-- Bootstrap 3.3.6 -->
     <link rel="stylesheet" href={{ asset("/lte/css/bootstrap.min.css") }}>
     <!-- Pnotify -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pnotify/3.0.0/pnotify.css">
+    <link href="//cdn.bootcss.com/pnotify/3.0.0/pnotify.css" rel="stylesheet">
     <link rel="stylesheet" href={{ asset("/css/pnotify.buttons.css") }}>
 
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link href="//cdn.bootcss.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
     <!-- Sweetalert -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/sweetalert2/4.0.5/sweetalert2.css">
+    <link href="//cdn.bootcss.com/limonte-sweetalert2/4.0.5/sweetalert2.min.css" rel="stylesheet">
     <!-- Theme style -->
     <link rel="stylesheet" href={{ asset("/lte/css/AdminLTE.css") }}>
     <link rel="stylesheet" href={{ asset("/lte/css/skin-blue.min.css") }}>
@@ -37,10 +37,10 @@
 {{-- jQuery --}}
 <script src={{ asset("/lte/js/jQuery-2.2.0.min.js") }}></script>
 {{-- pnotify --}}
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pnotify/3.0.0/pnotify.js"></script>
+<script src="//cdn.bootcss.com/pnotify/3.0.0/pnotify.js"></script>
 <script src={{ asset("/js/pnotify.buttons.js") }}></script>
 {{-- Sweetalert --}}
-<script src="https://cdn.jsdelivr.net/sweetalert2/4.0.5/sweetalert2.min.js"></script>
+<script src="//cdn.bootcss.com/limonte-sweetalert2/4.0.5/sweetalert2.min.js"></script>
 <!-- Bootstrap 3.3.6 -->
 <script src={{ asset("/lte/js/bootstrap.min.js") }}></script>
 {{-- AdminLTE js --}}
@@ -51,7 +51,7 @@
 <!-- Live Reload -->
 @if ( Config::get('app.debug') )
     <script type="text/javascript">
-        document.write('<script src="//localhost:35729/livereload.js?snipver=1" type="text/javascript"><\/script>')
+        // document.write('<script src="//localhost:35729/livereload.js?snipver=1" type="text/javascript"><\/script>')
     </script>
 @endif
 </body>
